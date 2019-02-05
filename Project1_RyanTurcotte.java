36a0e70419a0f041e47410a02b543890ca033578
Ryan Turcotte <ryanturcotte@ou.edu>
2019-02-05 12:50:12 -0600

    This is an example

---
+++ Project1_RyanTurcotte.java
@@ -0,0 +1,17 @@
+/** This program prints out fun facts about CS.
+ * @author Ryan Turcotte 
+ * @version 1.0
+ * I copied this code from the Project 1 Handout, butit's OK because Dr. Trytten
+ * told me to do it.
+ *
+ */
+public class Project1_RyanTurcotte {
+
+	// This is the main program. The instructions here 
+	// are what is done when the program is run
+	public static void main(String[] args)
+	{ 
+		System.out.println("Computing jobs are here to stay, " 
+				+ "regardless of where you are located.");
+	}
+}
